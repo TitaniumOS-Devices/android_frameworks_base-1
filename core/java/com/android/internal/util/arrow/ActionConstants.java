DotOS/android_frameworks_base

base: import custom utils
Change-Id: I5a167cbc4bce32a48bd0b65da6b2683daa905784

core/java/com/android/internal/util/dotos/gzosp/ActionConstants.java
@@ -1,0 +1,102 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.dotos;
+
+public class ActionConstants {
+
+    // key must fit with the values arrays from Settings to use
+    // SlimActions.java actions
+    public static final String ACTION_HOME                 = "**home**";
+    public static final String ACTION_BACK                 = "**back**";
+    public static final String ACTION_SEARCH               = "**search**";
+    public static final String ACTION_VOICE_SEARCH         = "**voice_search**";
+    public static final String ACTION_MENU                 = "**menu**";
+    public static final String ACTION_MENU_BIG             = "**menu_big**";
+    public static final String ACTION_POWER                = "**power**";
+    public static final String ACTION_NOTIFICATIONS        = "**notifications**";
+    public static final String ACTION_RECENTS              = "**recents**";
+    public static final String ACTION_SCREENSHOT           = "**screenshot**";
+    public static final String ACTION_IME                  = "**ime**";
+    public static final String ACTION_LAST_APP             = "**lastapp**";
+    public static final String ACTION_KILL                 = "**kill**";
+    public static final String ACTION_ASSIST               = "**assist**";
+    public static final String ACTION_VIB                  = "**ring_vib**";
+    public static final String ACTION_SILENT               = "**ring_silent**";
+    public static final String ACTION_VIB_SILENT           = "**ring_vib_silent**";
+    public static final String ACTION_POWER_MENU           = "**power_menu**";
+    public static final String ACTION_TORCH                = "**torch**";
+    public static final String ACTION_EXPANDED_DESKTOP     = "**expanded_desktop**";
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+    public static final String ACTION_KEYGUARD_SEARCH      = "**keyguard_search**";
+    public static final String ACTION_PIE                  = "**pie**";
+    public static final String ACTION_NAVBAR               = "**nav_bar**";
+    public static final String ACTION_IME_NAVIGATION_LEFT  = "**ime_nav_left**";
+    public static final String ACTION_IME_NAVIGATION_RIGHT = "**ime_nav_right**";
+    public static final String ACTION_IME_NAVIGATION_UP    = "**ime_nav_up**";
+    public static final String ACTION_IME_NAVIGATION_DOWN  = "**ime_nav_down**";
+    public static final String ACTION_CAMERA               = "**camera**";
+    public static final String ACTION_MEDIA_PREVIOUS       = "**media_previous**";
+    public static final String ACTION_MEDIA_NEXT           = "**media_next**";
+    public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
+    public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
+
+    // no action
+    public static final String ACTION_NULL            = "**null**";
+
+    // this shorcut constant is only used to identify if the user
+    // selected in settings a custom app...after it is choosed intent uri
+    // is saved in the ButtonConfig object
+    public static final String ACTION_APP          = "**app**";
+
+    public static final String ICON_EMPTY = "empty";
+    public static final String SYSTEM_ICON_IDENTIFIER = "system_shortcut=";
+    public static final String ACTION_DELIMITER = "|";
+
+    public static final String NAVIGATION_CONFIG_DEFAULT =
+          ACTION_BACK    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_HOME    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_RECENTS + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String NAV_RING_CONFIG_DEFAULT =
+          ACTION_ASSIST + ACTION_DELIMITER
+        + ACTION_NULL   + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String PIE_SECOND_LAYER_CONFIG_DEFAULT =
+          ACTION_POWER_MENU    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_NOTIFICATIONS + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SEARCH        + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SCREENSHOT    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_IME           + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+}

core/java/com/android/internal/util/dotos/gzosp/ConfigSplitHelper.java
@@ -1,0 +1,98 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.dotos;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ConfigSplitHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static ArrayList<ActionConfig> getActionConfigValues(Context context, String config,
+                String values, String entries, boolean isShortcut) {
+        // init vars to fill with them later the config values
+        int counter = 0;
+        ArrayList<ActionConfig> actionConfigList = new ArrayList<ActionConfig>();
+        ActionConfig actionConfig = null;
+
+        PackageManager pm = context.getPackageManager();
+        Resources settingsResources = null;
+        try {
+            settingsResources = pm.getResourcesForApplication(SETTINGS_METADATA_NAME);
+        } catch (Exception e) {
+            Log.e("ConfigSplitHelper", "can't access settings resources",e);
+        }
+
+        // Split out the config to work with and add to the list
+        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+            counter++;
+            if (counter == 1) {
+                actionConfig = new ActionConfig(configValue,
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries), null, null, null);
+            }
+            if (counter == 2) {
+                if (isShortcut) {
+                    actionConfig.setIcon(configValue);
+                    actionConfigList.add(actionConfig);
+                    //reset counter due that shortcut iteration of one action is finished
+                    counter = 0;
+                } else {
+                    actionConfig.setLongpressAction(configValue);
+                    actionConfig.setLongpressActionDescription(
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries));
+                }
+            }
+            if (counter == 3) {
+                actionConfig.setIcon(configValue);
+                actionConfigList.add(actionConfig);
+                //reset counter due that iteration of full config action is finished
+                counter = 0;
+            }
+        }
+
+        return actionConfigList;
+    }
+
+    public static String setActionConfig(
+            ArrayList<ActionConfig> actionConfigs, boolean isShortcut) {
+        String finalConfig = "";
+        ActionConfig actionConfig;
+
+        for (int i = 0; i < actionConfigs.size(); i++) {
+            if (i != 0) {
+                finalConfig += ActionConstants.ACTION_DELIMITER;
+            }
+            actionConfig = actionConfigs.get(i);
+            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            if (!isShortcut) {
+                finalConfig += actionConfig.getLongpressAction()
+                    + ActionConstants.ACTION_DELIMITER;
+            }
+            finalConfig += actionConfig.getIcon();
+        }
+
+        return finalConfig;
+    }
+
+}

core/java/com/android/internal/util/dotos/gzosp/Helpers.java
@@ -1,0 +1,30 @@
+package com.android.internal.util.dotos;
+
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class Helpers {
+    // avoids hardcoding the tag
+    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
+
+    public static void restartSystemUI(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            IActivityManager amn = ActivityManagerNative.getDefault();
+            context.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+            am.killBackgroundProcesses("com.android.systemui");
+            for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                if ("com.android.systemui".equals(app.processName)) {
+                    amn.killApplicationProcess(app.processName, app.uid);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

TitaniumOS-Devices/android_frameworks_base-1

core/java/com/android/internal/util/arrow/ActionConstants.java
@@ -1,0 +1,102 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.arrow;
+
+public class ActionConstants {
+
+    // key must fit with the values arrays from Settings to use
+    // SlimActions.java actions
+    public static final String ACTION_HOME                 = "**home**";
+    public static final String ACTION_BACK                 = "**back**";
+    public static final String ACTION_SEARCH               = "**search**";
+    public static final String ACTION_VOICE_SEARCH         = "**voice_search**";
+    public static final String ACTION_MENU                 = "**menu**";
+    public static final String ACTION_MENU_BIG             = "**menu_big**";
+    public static final String ACTION_POWER                = "**power**";
+    public static final String ACTION_NOTIFICATIONS        = "**notifications**";
+    public static final String ACTION_RECENTS              = "**recents**";
+    public static final String ACTION_SCREENSHOT           = "**screenshot**";
+    public static final String ACTION_IME                  = "**ime**";
+    public static final String ACTION_LAST_APP             = "**lastapp**";
+    public static final String ACTION_KILL                 = "**kill**";
+    public static final String ACTION_ASSIST               = "**assist**";
+    public static final String ACTION_VIB                  = "**ring_vib**";
+    public static final String ACTION_SILENT               = "**ring_silent**";
+    public static final String ACTION_VIB_SILENT           = "**ring_vib_silent**";
+    public static final String ACTION_POWER_MENU           = "**power_menu**";
+    public static final String ACTION_TORCH                = "**torch**";
+    public static final String ACTION_EXPANDED_DESKTOP     = "**expanded_desktop**";
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+    public static final String ACTION_KEYGUARD_SEARCH      = "**keyguard_search**";
+    public static final String ACTION_PIE                  = "**pie**";
+    public static final String ACTION_NAVBAR               = "**nav_bar**";
+    public static final String ACTION_IME_NAVIGATION_LEFT  = "**ime_nav_left**";
+    public static final String ACTION_IME_NAVIGATION_RIGHT = "**ime_nav_right**";
+    public static final String ACTION_IME_NAVIGATION_UP    = "**ime_nav_up**";
+    public static final String ACTION_IME_NAVIGATION_DOWN  = "**ime_nav_down**";
+    public static final String ACTION_CAMERA               = "**camera**";
+    public static final String ACTION_MEDIA_PREVIOUS       = "**media_previous**";
+    public static final String ACTION_MEDIA_NEXT           = "**media_next**";
+    public static final String ACTION_MEDIA_PLAY_PAUSE     = "**media_play_pause**";
+    public static final String ACTION_WAKE_DEVICE          = "**wake_device**";
+
+    // no action
+    public static final String ACTION_NULL            = "**null**";
+
+    // this shorcut constant is only used to identify if the user
+    // selected in settings a custom app...after it is choosed intent uri
+    // is saved in the ButtonConfig object
+    public static final String ACTION_APP          = "**app**";
+
+    public static final String ICON_EMPTY = "empty";
+    public static final String SYSTEM_ICON_IDENTIFIER = "system_shortcut=";
+    public static final String ACTION_DELIMITER = "|";
+
+    public static final String NAVIGATION_CONFIG_DEFAULT =
+          ACTION_BACK    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_HOME    + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY     + ACTION_DELIMITER
+        + ACTION_RECENTS + ACTION_DELIMITER
+        + ACTION_NULL    + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String NAV_RING_CONFIG_DEFAULT =
+          ACTION_ASSIST + ACTION_DELIMITER
+        + ACTION_NULL   + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+    public static final String PIE_SECOND_LAYER_CONFIG_DEFAULT =
+          ACTION_POWER_MENU    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_NOTIFICATIONS + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SEARCH        + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_SCREENSHOT    + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY           + ACTION_DELIMITER
+        + ACTION_IME           + ACTION_DELIMITER
+        + ACTION_NULL          + ACTION_DELIMITER
+        + ICON_EMPTY;
+
+}

core/java/com/android/internal/util/arrow/ConfigSplitHelper.java
@@ -1,0 +1,98 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.arrow;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ConfigSplitHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static ArrayList<ActionConfig> getActionConfigValues(Context context, String config,
+                String values, String entries, boolean isShortcut) {
+        // init vars to fill with them later the config values
+        int counter = 0;
+        ArrayList<ActionConfig> actionConfigList = new ArrayList<ActionConfig>();
+        ActionConfig actionConfig = null;
+
+        PackageManager pm = context.getPackageManager();
+        Resources settingsResources = null;
+        try {
+            settingsResources = pm.getResourcesForApplication(SETTINGS_METADATA_NAME);
+        } catch (Exception e) {
+            Log.e("ConfigSplitHelper", "can't access settings resources",e);
+        }
+
+        // Split out the config to work with and add to the list
+        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+            counter++;
+            if (counter == 1) {
+                actionConfig = new ActionConfig(configValue,
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries), null, null, null);
+            }
+            if (counter == 2) {
+                if (isShortcut) {
+                    actionConfig.setIcon(configValue);
+                    actionConfigList.add(actionConfig);
+                    //reset counter due that shortcut iteration of one action is finished
+                    counter = 0;
+                } else {
+                    actionConfig.setLongpressAction(configValue);
+                    actionConfig.setLongpressActionDescription(
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries));
+                }
+            }
+            if (counter == 3) {
+                actionConfig.setIcon(configValue);
+                actionConfigList.add(actionConfig);
+                //reset counter due that iteration of full config action is finished
+                counter = 0;
+            }
+        }
+
+        return actionConfigList;
+    }
+
+    public static String setActionConfig(
+            ArrayList<ActionConfig> actionConfigs, boolean isShortcut) {
+        String finalConfig = "";
+        ActionConfig actionConfig;
+
+        for (int i = 0; i < actionConfigs.size(); i++) {
+            if (i != 0) {
+                finalConfig += ActionConstants.ACTION_DELIMITER;
+            }
+            actionConfig = actionConfigs.get(i);
+            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            if (!isShortcut) {
+                finalConfig += actionConfig.getLongpressAction()
+                    + ActionConstants.ACTION_DELIMITER;
+            }
+            finalConfig += actionConfig.getIcon();
+        }
+
+        return finalConfig;
+    }
+
+}

core/java/com/android/internal/util/arrow/Helpers.java
@@ -1,0 +1,30 @@
+package com.android.internal.util.arrow;
+
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class Helpers {
+    // avoids hardcoding the tag
+    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
+
+    public static void restartSystemUI(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            IActivityManager amn = ActivityManagerNative.getDefault();
+            context.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+            am.killBackgroundProcesses("com.android.systemui");
+            for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                if ("com.android.systemui".equals(app.processName)) {
+                    amn.killApplicationProcess(app.processName, app.uid);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}