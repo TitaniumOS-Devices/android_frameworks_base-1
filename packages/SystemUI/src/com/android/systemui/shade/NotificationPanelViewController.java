adbed7e9b8e26dd70f2c2b1d112b982823dd45b4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-26 03:22:20 +0530

    SystemUI: Combined QS header fixes
    Fixes:
    - Too low margin above QQS
    - Missing signal icons in QQS when >1 SIM
    - Privacy chip not clickable in QS
    - Too low padding between clock and date in QQS
    
    Change-Id: Ice4ddb8fab4542d386e65818c58172d1d30c03ef

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2497,6 +2497,7 @@ public final class NotificationPanelViewController extends PanelViewController {
 
         mDepthController.setQsPanelExpansion(qsExpansionFraction);
         mStatusBarKeyguardViewManager.setQsExpansion(qsExpansionFraction);
+        mNotificationsQSContainerController.setQsExpandedFraction(qsExpansionFraction);
 
         // updateQsExpansion will get called whenever mTransitionToFullShadeProgress or
         // mLockscreenShadeTransitionController.getDragProgress change.
